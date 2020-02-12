291cd80d575f74054a6ad0e3e38d96691afbda63
bulat <bulzhamur@gmail.com>
2020-02-12 21:57:44 +0800

    vuex mutations begin

--- src/main/resources/static/js/store/store.js
+++ src/main/resources/static/js/store/store.js
@@ -12,8 +12,39 @@ export default new Vuex.Store({
         sortedMessages: state => state.messages.sort((a, b) => -(a.id - b.id))
     },
     mutations: {
-        increment (state) {
-            state.count++
+        addMessageMutation(state, message) {
+            state.messages = [
+                ...state.messages,
+                message
+            ]
+        },
+        updateMessageMutation(state, message) {
+            const updateIndex = state.messages.findIndex(item => item.id === message.id)
+            state.messages = [
+                ...state.messages.slice(0, updateIndex),
+                message,
+                ...state.messages.slice(updateIndex + 1)
+            ]
+        },
+        removeMessageMutation(state, message) {
+            const delIndex = state.messages.findIndex(item => item.id === message.id)
+            if(delIndex > -1){
+                state.messages = [
+                    ...state.slice(0, delIndex),
+                    ...state.messages(delIndex + 1)
+                ]
+            }
         }
+    },
+    actions: {
+        async addMessageAction({commit}, message) {
+
+        },
+        async updateMessageAction({commit}, message) {
+
+        },
+        async removeMessageAction({commit}, message) {
+
+        },
     }
 })
